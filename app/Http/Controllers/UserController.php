bc32bc7b5875e0f61605df6afdbed2a8f1dc9b95
AnggaNellow <anggazxcasd1@gmail.com>
2024-02-18 22:45:29 +0700

    feat: validate register

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,7 +6,12 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function register() {
+    public function register(Request $request) {
+        $incomingFields = $request->validate([
+            'name' => ['required', 'min:3', 'max:20'],
+            'email' => ['required', 'email', 'min:3'],
+            'password' => ['required', 'min:5', 'max:200']
+        ]);
         return 'Thank you for registering!';
     }
 }
